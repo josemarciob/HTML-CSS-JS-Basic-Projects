cfe8a5ab0fe9c993df2b08175d20996d28f341e5
José Marcio <jose.souza.24716@a.fecaf.com.br>
2022-10-23 17:58:39 -0300

    Update app.js, comments

--- JS/app.js
+++ JS/app.js
@@ -3,6 +3,7 @@ const links = document.querySelector('.links')
 const navToggle = document.querySelector('.nav-toggle')
 
 //EVENTS
+/* OPEN LINK LIST */
 navToggle.addEventListener('click', function () {
   links.classList.toggle('show-links')
 })
